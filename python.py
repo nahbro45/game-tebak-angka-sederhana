54443a4b2ddca61d180ca37aa59f41ae7946b8c5
nahbro45 <daffazaky910@gmail.com>
2025-08-18 17:32:45 +0700

    Update python.py
    NEW UPDATE FIX BUG

--- python.py
+++ python.py
@@ -1,23 +1,40 @@
 import random 
 
 print('/////////////////TEBAK ANGKA ////////////////')
-print('__________-ANDA PUNYA 5 KESEMPATAN UNTUK MENEBAK______________')
-print('=========angka di pilih oleh computer============')
-print('----------jadi kamu akan bermain dengan komputer')
+print('|___________________________________________|')
+print('                                             ')
+print('^> cara bermain')
+print('-------------------------------------------------------')
+print('^> kalinn akan bermain dengan komputer ')
+print('-------------------------------------------------------')
+print('^> ANDA PUNYA 5 KESEMPATAN UNTUK MENEBAK')
+print('-------------------------------------------------------')
+print("^> komputer akan menjawab dengan input (komputer: )")
+print('--------------------------------------------------------')
+print(' ')
+print('===== selamat bermain dan semoga beruntung =========')
+print('----------------------------------------------------')
 
 c = random.randint(1,10)
 r = 0
 max_r = 5   
 
 while(r < max_r):
-    b = int(input('komputer: pilih angka 1 - 10 = '))
+    b = int(input('komputer: pilih angka 1 - 10= '))
     r += 1 
 
     if ( b < c ):
         print('komputer: terlalu kecil ')
+        print('                           ')
     elif( b > c ):
-        print ('kompuuter: terlalu besar ')
-    elif(b == c ):
-        print('//////////kamu benar///////// ')        
-if(r == max_r) and ( b != c ):
-    print('komputer: kesempatanmu habis jawaban yang benar adalah' + str({c}))
+        print ('komputer: terlalu besar ')
+        print('                               ')
+    else:
+        print('komputer:  selamat kamu benar ')
+        print('KAMU MANDAPAT 3 BINTANG')
+        exit(0)
+    print('kesempatan anda tinggal: ' + str(max_r - r))
+    print('-------------------------------------------------')
+    print('     ')
+print('anda gagal ')
+print('mision filed')
