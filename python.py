f746fba895130255074d74f5a7ad7692cb207c07
nahbro45 <daffazaky910@gmail.com>
2025-08-18 16:42:56 +0700

    Update python.py
    update litlebit

--- python.py
+++ python.py
@@ -2,20 +2,22 @@ import random
 
 print('/////////////////TEBAK ANGKA ////////////////')
 print('__________-ANDA PUNYA 5 KESEMPATAN UNTUK MENEBAK______________')
+print('=========angka di pilih oleh computer============')
+print('----------jadi kamu akan bermain dengan komputer')
 
 c = random.randint(1,10)
 r = 0
 max_r = 5   
 
 while(r < max_r):
-    b = int(input('pilih angka 1 - 10= '))
+    b = int(input('komputer: pilih angka 1 - 10 = '))
     r += 1 
 
     if ( b < c ):
-        print('terlalu kecil ')
+        print('komputer: terlalu kecil ')
     elif( b > c ):
-        print ('terlalu besar ')
+        print ('kompuuter: terlalu besar ')
     elif(b == c ):
         print('//////////kamu benar///////// ')        
 if(r == max_r) and ( b != c ):
-    print('kesempatanmu habis jawaban yang benar adalah' + str({c}))
+    print('komputer: kesempatanmu habis jawaban yang benar adalah' + str({c}))
